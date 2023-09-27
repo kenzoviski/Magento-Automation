4ee1d2db05e8b0d57c984186eb23e2ca69bc106e
TA <ta@mail.com>
2023-09-27 09:24:56 +0100

    CleanUp

--- utils/TitlePage-deprecatedFunction.ts
+++
@@ -1,6 +0,0 @@
-// import { Page } from '@playwright/test';
-
-// export async function assertPageTitle(page: Page, expectedTitle: string): Promise<boolean> {
-//   const actualTitle = await page.title();
-//   return actualTitle === expectedTitle;
-// }
